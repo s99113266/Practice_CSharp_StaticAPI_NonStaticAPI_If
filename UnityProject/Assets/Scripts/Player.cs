9836f3c13ca98a35c81e4b59d4b6c2838da2909f
s99113266 <41054903+s99113266@users.noreply.github.com>
2019-11-28 23:41:16 +0800

    作業4
    僅模仿出效果，架構很鬆散，對於欄位的應用要加強

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("玩家HP")]
+    public float PlayerHP = 100f;
+
+
+    [Header("玩家攻擊力")]
+    public float PlayerATK;
+
+    [Header("殭屍")]
+    public GameObject goZombie;
+
+    [Header("音源")]
+    public AudioSource PlayerAudioSource;
+
+    [Header("音效")]
+    public AudioClip PlayerAudioClip;
+    public void PlayerAttacked(float PAValue)
+    {
+
+        if (goZombie.GetComponent<Zombie>().ZombieHP > 0)
+        {
+            goZombie.GetComponent<Zombie>().ZombieHP -= PAValue;
+            PlayerAudioSource.clip = PlayerAudioClip;
+            PlayerAudioSource.Play();
+            print("殭屍受到傷害:" + PAValue);
+            print("殭屍剩餘血量:" + goZombie.GetComponent<Zombie>().ZombieHP);
+        }
+        if(goZombie.GetComponent<Zombie>().ZombieHP <= 0)
+        {
+            print("殭屍已經死亡");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (PlayerHP <= 0) return;
+            PlayerATK = Random.Range(0.1f, 50f);
+            PlayerAttacked(PlayerATK);
+        }
+    }
+}
---
+++ UnityProject/Assets/Scripts/Zombie.cs
@@ -0,0 +1,52 @@
+﻿
+
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+    [Header("殭屍HP")]
+    public float ZombieHP = 100f;
+
+    [Header("殭屍攻擊力")]
+    public float ZombieATK;
+
+    [Header("玩家")]
+    public GameObject goPlayer;
+
+    [Header("音源")]
+    public AudioSource ZombieAudioSource;
+
+    [Header("音效")]
+    public AudioClip ZombieAudioClip;
+
+
+    public void ZombieAttacked(float ZAAttacked)
+    {
+
+        if (goPlayer.GetComponent<Player>().PlayerHP > 0)
+        {
+            goPlayer.GetComponent<Player>().PlayerHP -= ZAAttacked;
+            ZombieAudioSource = GetComponent<AudioSource>();
+            ZombieAudioSource.clip = ZombieAudioClip;
+            ZombieAudioSource.Play();
+
+            print("殭屍受到傷害:" + ZAAttacked);
+            print("殭屍剩餘血量:" + goPlayer.GetComponent<Player>().PlayerHP);
+        }
+        if(goPlayer.GetComponent<Player>().PlayerHP <= 0)
+        {
+            print("小屁還已經屁死了");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (ZombieHP <= 0) return;
+            ZombieATK = Random.Range(0.1f, 50f);
+            ZombieAttacked(ZombieATK);
+        }
+    }
+}
